5d20fe6e2d4d63674ce796524e4d23f7109f2347
Coloradik <107196602+Coloradik@users.noreply.github.com>
2025-05-26 15:32:32 +0300

    Delete Єва Івасик 11-А HTML-CSS directory

--- Єва Івасик 11-А HTML-CSS/js/modal.js
+++
@@ -1,8 +0,0 @@
-const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.modal-btn-open');
-const modalBtnClose = document.querySelector('.modal-btn-close');
-
-const toogleModal = () => modal.classList.toggle('is-hidden');
-
-modalBtnOpen.addEventListener('click', toogleModal);
-modalBtnClose.addEventListener('click', toogleModal);
